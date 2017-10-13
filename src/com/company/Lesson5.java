69df784a1ed07ecd0a89cdf901cee2558acfa2ec
LolitaAslanova <lolita.aslanova@gmail.com>
2017-10-13 20:10:40 +0300

    Added

---
+++ src/com/company/Lesson5.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by UITSchoolHP-20 on 10/13/2017.
+ */
+public class Lesson5 {
+    public static void main(String[] args) {
+        char sym = 'h';
+        for(int i = 0; i<5; i++){
+            System.out.print(sym + " ");
+            sym++;
+        }
+    }
+}
